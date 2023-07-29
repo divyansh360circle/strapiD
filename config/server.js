747b96407bd133f7e521dfbbd44aaf0aab4da1b2
divyansh360circle <divyansh@360circleinfotech.com>
2023-07-29 14:06:10 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  url: env('http://strapi.qafto.in'),
+  app: { 
+    keys: env.array('APP_KEYS')
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+})
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
